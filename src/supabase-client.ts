4a58bd7971a9214e3bb70a21e2920e65ee58a952
leonardo-ga <leonardo.r.m.gasparro@gmail.com>
2025-12-12 01:18:52 +0100

    added NON-working supabase config files (only to remember how NOT to do)

---
+++ src/supabase-client.ts
@@ -0,0 +1,14 @@
+/**
+ * Moved to \src\app\core\services\supabase.service.ts
+ * so that it creates a unique istance
+ */
+
+/*import { createClient } from  '@supabase/supabase-js';
+
+const supabaseUrl = 'https://wphshfgaldlhwemutbmr.supabase.co';
+const supabasePublicKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndwaHNoZmdhbGRsaHdlbXV0Ym1yIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjUzOTIxNzAsImV4cCI6MjA4MDk2ODE3MH0.f3O6ZJLZJ51PDIejcjNeO0nyluKpsHN1aTb8S-LqqQE';
+
+export const supabase = createClient(
+    supabaseUrl,
+    supabasePublicKey
+);*/
---
+++ src/supabase-node-shim.d.ts
@@ -0,0 +1,11 @@
+/**
+ * This was needed to compile when we used supabase-client.ts
+ */
+
+/*declare class Buffer {
+  static from(data: any, encoding?: string): Buffer;
+}
+
+declare namespace NodeJS {
+    interface ReadableStream { }
+}*/
